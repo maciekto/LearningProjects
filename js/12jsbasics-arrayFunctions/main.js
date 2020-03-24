feb0f4929b17166379ea4a51196a30b8d73d3572
maciekto <ekstar007@gmail.com>
2020-03-24 17:25:09 +0100

    Learn Arrow functions (this keyword)

---
+++ js/12jsbasics-arrayFunctions/main.js
@@ -0,0 +1,26 @@
+ // Arrow functions
+class Doma{
+    constructor(hair, nails, eye){
+        this.hair = hair,
+        this.nails = nails,
+        this.eye = eye
+    }
+    Arrow(){
+        setTimeout(()=>{
+            console.log(this.hair);
+        },11)
+    }
+    getDoma(){
+        setTimeout(function(){
+            console.log(this.hair);
+        },11)
+    }
+}
+const DOMA = new Doma('black', 'not done', 'beautiful');
+
+ 
+
+DOMA.Arrow(); // arrow function takes her context in 'this' keyword
+DOMA.getDoma(); // normal function takes global context as 'this' keyword so its undefined
+
+
