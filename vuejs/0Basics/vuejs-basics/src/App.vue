dc4a2c50b68183e947aba166f518a53ee7957822
maciekto <ekstar007@gmail.com>
2020-04-10 20:41:07 +0200

    Added styles, know how to get location
    	- created separate components and styled it
    	- learn how to get location

--- vuejs/0Basics/vuejs-basics/src/App.vue
+++ vuejs/0Basics/vuejs-basics/src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <div id="app">
-    <Home />
-    <button @click="getLocation">VIEW YOUR LOCATION</button>
+  <div class="app">
+    <div class="app-Inner">
+      <Home />
+      <MainValues />
+      <Form />
+      <More />
+    </div>
+    <!-- <button @click="getLocation">VIEW YOUR LOCATION</button>
     <div>{{ width }}</div>
     <div class="location"></div>
+    <div class="map"></div>-->
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
+import MainValues from './components/MainValues.vue';
+import Form from './components/Form.vue';
+import More from './components/More.vue';
 
 export default {
   name: 'App',
   components: {
     Home,
+    MainValues,
+    Form,
+    More,
   },
   data() {
     return {
@@ -42,6 +54,31 @@ export default {
 </script>
 
 <style lang="scss">
+  body {
+    font-family: 'Fira Sans', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  .app {
+    background: url("../src/assets/bg.jpg") center center;
+    background-size: cover;
+    background-position: 30%;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-Inner{
+      width: 100%;
+      height: 70vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
   button {
     width: 200px;
     height: 50px;
--- vuejs/0Basics/vuejs-basics/src/components/Home.vue
+++ vuejs/0Basics/vuejs-basics/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h2 class="title">Hello</h2>
+    <h2 class="title">GOLDEN HOUR</h2>
   </div>
 </template>
 
@@ -12,3 +12,22 @@ export default {
 
 
 </script>
+
+<style lang="scss" scoped>
+  h2 {
+    font-weight: 800;
+    font-size: 30px;
+    letter-spacing: 1.5px;
+  }
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    width: 100%;
+    height: 20vh;
+    padding: 20px;
+    text-align: center;
+    color: white;
+  }
+</style>
