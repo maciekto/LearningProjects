06dfb025a8a91a2a08423c649bc716d3000c4f80
maciekto <ekstar007@gmail.com>
2020-04-28 19:24:10 +0200

    Learn about v-bind, and v-model again, computed, and watchers

--- vuejs/1Learn/vue-learn/src/components/Form.vue
+++ vuejs/1Learn/vue-learn/src/components/Form.vue
@@ -1,20 +1,9 @@
 <template>
     <main class="Form">
-        <label>Login:<br />
-            <input type="text" v-model="login">
-        </label>
-        <label>Repeat login:<br />
-            <input type="text">
-        </label>
-        <label>E-mail:<br />
-            <input type="text">
-        </label>
-        <label>Repeat e-mail:<br />
-            <input type="text">
-        </label>
-        <label><br />
-            <input type="submit">
-        </label>
+        <h2 :class="givecolor">{{ login }}</h2>
+        Type anything <input type="text" v-model.lazy="login"> <br /><br />
+        Type 'yellow', 'blue', or 'red <input type="text" v-model="givecolor">
+        <h2>{{ computedTitle }}</h2>
     </main>
 </template>
 <script>
@@ -22,50 +11,31 @@ export default {
   name: 'Form',
   data() {
     return {
-      login: 'give login',
+      login: 'Hello',
+      givecolor: 'red',
     };
+  },
+  computed: {
+    computedTitle() {
+      return this.login;
+    },
+  },
+  watch: {
+    login() {
+      alert(`Login was changed to ${this.login}`);
+    },
   },
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    .Form{
-        border: 1px solid black;
-        box-shadow: 0px 0px 5px black;
-        border-radius: 4px;
-        width: 50vw;
-        height: 60vw;
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        flex-direction: column;
-        text-align: center;
+    .red{
+        color: red;
     }
-    input{
-        border-radius: 5px;
-        width: 25vw;
-        transition: box-shadow 0.4s;
-        border: none;
-        border-bottom: 1px solid black;
-        outline: none;
+    .blue{
+        color: blue;
     }
-    input[type='submit']{
-        border-radius: 5px;
-        border-bottom: 1px solid black;
-        width: 10vh;
-        padding: 5px;
-    }
-    input[type='submit']:focus{
-        transition: 0.4s;
-        border: 1px solid black;
-        box-shadow: none;
-    }
-    input:focus{
-        transition: 0.4s;
-        box-shadow: 0px 2px rgba($color: #000, $alpha: 0.3);
-    }
-    input:hover{
-        transition: 0.4s;
-        box-shadow: 0px 0px 3px black;
+    .yellow{
+        color: yellow;
     }
 </style>
