98b1cc4a33e6b98f93af0fb8c10849472a539bed
maciekto <ekstar007@gmail.com>
2020-04-29 19:20:48 +0200

    Learn v-if, v-for, scss scoped

--- vuejs/1Learn/vue-learn/src/components/Form.vue
+++ vuejs/1Learn/vue-learn/src/components/Form.vue
@@ -1,9 +1,21 @@
 <template>
     <main class="Form">
+      <div class="Form-First Form-Block">
         <h2 :class="givecolor">{{ login }}</h2>
         Type anything <input type="text" v-model.lazy="login"> <br /><br />
         Type 'yellow', 'blue', or 'red <input type="text" v-model="givecolor">
         <h2>{{ computedTitle }}</h2>
+      </div>
+      <div class="Form-Second Form-Block">
+        <input type="checkbox" v-model="checkbox">
+        <span v-if="checkbox === true">YOU CHECKED THAT</span>
+        <span v-else>NOT CHECKED</span><br /><br />
+        <input type="text" v-model="message">
+        <span v-if="message === 'hello'">You said {{ message }}</span>
+        <span v-else-if="message === 'goodbye'">You said {{ message }}</span>
+        <span v-else-if="message === 'lol'">You said {{ message }}</span>
+        <span v-else>U said something diferent</span>
+      </div>
     </main>
 </template>
 <script>
@@ -13,6 +25,8 @@ export default {
     return {
       login: 'Hello',
       givecolor: 'red',
+      checkbox: false,
+      message: '',
     };
   },
   computed: {
@@ -37,5 +51,8 @@ export default {
     }
     .yellow{
         color: yellow;
+    }
+    .Form-Block{
+      margin: 50px;
     }
 </style>
